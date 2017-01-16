5c8908ee003f42b0158df6c6efe7d2df43905128
sIeepy <biegacz1993@wp.pl>
2017-01-16 09:31:03 -0800

    lel

---
+++ test/controllers/index_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class IndexControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get index_home_url
+    assert_response :success
+    assert_select "title", "Home"
+  end
+
+end
