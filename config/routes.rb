2e1978910cac411124c175097060af8c6cf5b8d0
sleepy <biegacz1993@wp.pl>
2017-02-20 20:24:12 +0100

    simple login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'index#home'
   get 'index/home'
   get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
 
