773989bee9c9545efb573acac8bc75794f16d0fb
sleepy <biegacz1993@wp.pl>
2017-03-12 17:05:32 +0100

    create db

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,8 +1,28 @@
 class DatabasesController < ApplicationController
+  include DatabasesHelper
   def new
-    @database= User.database.new
+    @database = Database.new :db_owner => current_user.name
+  end
+
+  def create
+    @database = Database.new(database_params)
+    if @database.save
+      redirect_to @database
+    else
+      render 'new'
+    end
   end
 
   def edit
+  end
+
+  def show
+      @database = Database.find(params[:id])
+  end
+
+  private
+
+  def database_params
+    params.require(:database).permit(:db_name, :db_owner)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/create', to: 'databases#new'
+  post '/create', to: 'databases#create'
   get '/edit', to: 'databases#edit'
   resources :users
+  resources :databases
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
