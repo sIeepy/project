e6325b710d12314d946dd1826864d92084b04a23
sleepy <biegacz1993@wp.pl>
2017-03-28 20:38:55 +0200

    db creation

--- app/services/new_role.rb
+++ app/services/new_role.rb
@@ -1,22 +1,21 @@
 class NewRole
 
-    def initialize(user_id)
-      ur_id = User.find user_id
-      @owner = ur_id.name
+  def initialize(user_id)
+    ur_id = User.find user_id
+    @owner = ur_id.name
+  end
+
+  def chcek
+   end
+
+  def create_role
+    check = "SELECT 1 FROM pg_roles WHERE rolname = '#{@owner}'"
+    result = ActiveRecord::Base.connection.exec_query(check)
+    if result.present?
+      return result
+    else
+      form =  "CREATE ROLE #{@owner} CREATEDB"
+      ActiveRecord::Base.connection.execute(form)
     end
-
-    def chcek
-     end
-
-    def create_role
-        form = "DO $$
-                  BEGIN
-                    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = '#{@owner}') THEN
-                      CREATE ROLE #{@owner} CREATEDB;
-                    END IF;
-                  END
-                $$;"
-
-        ActiveRecord::Base.connection.execute(form)
-    end
+  end
 end
