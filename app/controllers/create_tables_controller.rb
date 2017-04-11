e37d242f9ad2f2b7de3e01c28d180698afb34522
sleepy <biegacz1993@wp.pl>
2017-04-11 20:17:38 +0200

    save

--- app/controllers/create_tables_controller.rb
+++ app/controllers/create_tables_controller.rb
@@ -1,6 +1,6 @@
 class CreateTablesController < ApplicationController
   skip_before_action :verify_authenticity_token
-  def table
+  def new
     CreateTable.new.new_table
     redirect_to database_url(id: current_database.id)
   end
