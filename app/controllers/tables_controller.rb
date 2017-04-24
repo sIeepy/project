03b3b6f36cc98398fb493cd7bcbc5d75f5a31a0f
sleepy <biegacz1993@wp.pl>
2017-04-24 21:53:46 +0200

    create table

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,18 +1,24 @@
 class TablesController < ApplicationController
-    skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   def new
-      @table = Table.new
+    @table = Table.new
   end
 
   def create
     @table = Table.new(table_params)
-      respond_to do |format|
+    respond_to do |format|
       if @table.valid?
-          format.html { redirect_to success_path, notice: 'bla.' }
+        ur_id = current_user.id
+        db_id = current_database.db_name
+        t_n = params[:table][:table_name]
+        r_n = params[:table][:row_1]
+        d_n = params[:table][:data_1]
+        CreateTable.new(db_id, ur_id, t_n, r_n, d_n).new_table
+        format.html { redirect_to success_path, notice: 'Feedback' }
       else
-          format.html { render :new }
+        format.html { render :new }
       end
-      end
+    end
   end
 
    def success
@@ -20,7 +26,7 @@ class TablesController < ApplicationController
 
   private
 
-   def table_params
-    params.require(:table).permit(:name, :email)
-   end
+  def table_params
+    params.require(:table).permit(:table_name, :row_1, :data_1)
+  end
 end
--- app/services/create_table.rb
+++ app/services/create_table.rb
@@ -1,14 +1,16 @@
 class CreateTable < ApplicationController
-  def initialize(database, user)
+  def initialize(database, user, t_name, r1_name, d1)
+    @d1 = d1
+    @t_name = t_name
+    @r1_name = r1_name
     @database = database
     @user = user
   end
 
   def new_table
     ConnectDatabase.new(@database, @user).connection
-    #create_table = "CREATE TABLE awsseale (
-    #                          did     integer,
-    #                          name    varchar(40) UNIQUE);"
-    #RemoteConnect.connection.execute(create_table)
+    create_table = "CREATE TABLE #{@t_name} (
+                              #{@r1_name}     #{@d1});"
+    RemoteConnect.connection.execute(create_table)
   end
 end
