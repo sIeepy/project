a05db03cd68a37e5b546849615187e09fba01547
sleepy <biegacz1993@wp.pl>
2017-05-09 23:33:49 +0200

    update table

--- app/services/edit_table.rb
+++ app/services/edit_table.rb
@@ -5,7 +5,6 @@ class EditTable
     @table_name = table[:table_name]
     @table_name_old = table[:old_t]
     split(table)
-    binding.pry
   end
 
   def split(table)
@@ -13,11 +12,38 @@ class EditTable
     table.values.each { |k, v| c1 << k }
     c1.shift
     c1.shift
-    @row = c1.map { |h| h['row'] }
+    @col = c1.map { |h| h['row'] }
     @data = c1.map { |h| h['data'] }
-    @old_r = c1.map { |h| h['old_r'] }
+    @old_c = c1.map { |h| h['old_r'] }
     @old_t = c1.map { |h| h['old_t'] }
   end
+
+  def empty_c(fresh, old)
+    tog = fresh.zip old
+    @new_a = tog.reject { |f, o| f.blank? }
+    unless @new_a.empty?
+      @fresh_a = []
+      @old_a = []
+      @new_a.each do |f, o|
+        @fresh_a << f
+        @old_a << o
+      end
+    end
+  end
+
+  def empty_d(fresh, col)
+    tog = fresh.zip col
+    @new_d = tog.reject { |f, c| f.blank? }
+    unless @new_d.empty?
+      @fresh_d = []
+      @col = []
+      @new_d.each do |f, c|
+        @fresh_d << f
+        @col << c
+      end
+    end
+  end
+
   def rename_table
     ConnectDatabase.new(@database, @user).connection
     if @table_name == ''
@@ -30,12 +56,51 @@ class EditTable
     end
   end
 
+  def column_name(table, col_n, col_o)
+    col_name = "ALTER TABLE #{table} RENAME COLUMN #{col_o} TO #{col_n};"
+    RemoteConnect.connection.execute(col_name)
+  end
+
+  def data_name(table, col, dat_n)
+    dat_name = "ALTER TABLE #{table} ALTER COLUMN #{col} TYPE #{dat_n} USING (#{col}::#{dat_n})"
+    RemoteConnect.connection.execute(dat_name)
+  end
+
   def rename_row
     ConnectDatabase.new(@database, @user).connection
-    if @table_name == ''
+    if @table_name != ''
+      check_one(@col, @old_c, @table_name)
+      check_two(@data, @table_name)
     else
-      rename_t = "ALTER TABLE distributors RENAME COLUMN address TO city;"
-      RemoteConnect.connection.execute(rename_t)
+      check_one(@col, @old_c, @table_name_old)
+      check_two(@data, @table_name_old)
+    end
+  end
+
+  def check_one(fresh, old, table)
+    empty_c(fresh, old)
+    unless @new_a.empty?
+      @new_a.size.times do |i|
+        column_name(table, @fresh_a[i], @old_a[i])
+      end
+    end
+  end
+
+  def check_two(fresh, table)
+    if @col.nil?
+      empty_d(fresh, @old_c)
+      unless @new_d.empty?
+        @new_d.size.times do |i|
+          data_name(table, @col[i], @fresh_d[i])
+        end
+      end
+    else
+      empty_d(fresh, @col)
+      unless @new_d.empty?
+        @new_d.size.times do |i|
+          data_name(table, @col[i], @fresh_d[i])
+        end
+      end
     end
   end
 end
